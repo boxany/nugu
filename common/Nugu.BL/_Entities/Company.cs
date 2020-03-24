8c74ab1feaddfb2f6bed68bdbfae268c0599f8f6
boxany <dev.hanypark@gmail.com>
2020-03-24 17:58:33 +0900

    Init proj

---
+++ common/Nugu.BL/_Entities/Company.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Nugu.BL
+{
+    public class Company
+    {
+        public Company()
+        {
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }   
+    }
+}
