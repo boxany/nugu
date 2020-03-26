6815d5c4fe358b159ef0d2aa871cf4ee5c90d9c1
boxany <dev.hanypark@gmail.com>
2020-03-26 18:27:55 +0900

    init proj~

--- common/Nugu.BL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Nugu.BL
-{
-    public class Class1
-    {
-    }
-}
--- common/Nugu.BL/_Entities/Company.cs
+++ common/Nugu.BL/_Entities/Company.cs
@@ -10,5 +10,6 @@ namespace Nugu.BL
 
         public Guid Id { get; set; }
         public string Name { get; set; }   
+        public DateTime CreatedDate { get; set; }
     }
 }
